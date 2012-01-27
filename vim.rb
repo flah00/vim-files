574ffdf4e7f04feb05d3b376c398d7b24cd7a7db
Philip Champon <philip@adaptly.com>
2012-01-27 17:37:09 -0500

    added brew file

---
+++ vim.rb
@@ -0,0 +1,27 @@
+require 'formula'
+ 
+class Vim < Formula
+  # Get stable versions from hg repo instead of downloading an increasing
+  # number of separate patches.
+  url 'https://vim.googlecode.com/hg/', :revision => '992b24149a9e'
+  version '7.3.420'
+  homepage 'http://www.vim.org/'
+ 
+  head 'https://vim.googlecode.com/hg/'
+ 
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "--enable-gui=no",
+                          "--with-x",
+                          "--disable-nls",
+                          "--enable-multibyte",
+                          "--with-tlib=ncurses",
+                          "--disable-pythoninterp",
+                          "--enable-rubyinterp",
+                          "--with-ruby-command=/usr/local/bin/ruby",
+                          "--with-features=huge"
+    system "make"
+    system "make install"
+  end
+end
