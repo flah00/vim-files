2f524b970e4f349c42371dce5e2705155982fe4e
Philip Champon <philip@adaptly.com>
2012-07-25 14:26:46 -0400

    up

--- vim.rb
+++ vim.rb
@@ -4,12 +4,12 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :revision => '70eff6af1158'
-  version '7.3.462'
-
+  url 'https://vim.googlecode.com/hg/', :revision => '6c318419e331'
+  version '7.3.515'
   head 'https://vim.googlecode.com/hg/'
 
   def install
+    user_dir = File.exists?("/Users/pchampon") ? "/Users/pchampon" : "/Users/flah"
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--enable-gui=no",
@@ -19,7 +19,7 @@ class Vim < Formula
                           "--with-tlib=ncurses",
                           "--enable-rubyinterp",
                           "--enable-cscope",
-                          "--with-ruby-command=/Users/flah/.rvm/rubies/ruby-1.9.2-p290/bin/ruby",
+                          "--with-ruby-command=#{user_dir}/.rvm/rubies/ruby-1.9.2-p290/bin/ruby",
                           "--with-features=huge"
     system "make"
     system "make install"
